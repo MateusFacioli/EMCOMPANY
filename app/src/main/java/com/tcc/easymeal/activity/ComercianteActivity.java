dd1ccdfc58532658a09e4b561c15fc951342b4e2
MateusFacioli <mateus_rf@hotmail.com>
2019-08-21 19:13:20 -0300

    toolbar padronizada

--- app/src/main/java/com/tcc/easymeal/activity/ComercianteActivity.java
+++ app/src/main/java/com/tcc/easymeal/activity/ComercianteActivity.java
@@ -263,6 +263,7 @@ public class ComercianteActivity extends AppCompatActivity implements OnMapReady
            });
            break;
        case 2:
+
            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                @Override
                public void onClick(DialogInterface dialog, int which) {
