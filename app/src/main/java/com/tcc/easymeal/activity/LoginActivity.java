08e7829685f90503adc26a97b579931af06d5283
leandrx11 <dellg7gaming>
2019-11-09 20:44:46 -0300

    Alteração de mensagem de autenticação na tela de login

--- app/src/main/java/com/tcc/easymeal/activity/LoginActivity.java
+++ app/src/main/java/com/tcc/easymeal/activity/LoginActivity.java
@@ -108,7 +108,7 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("TagLoginErrado", "Email ou Senha invalidos!", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "Erro! E-mail ou senha invalidos ",
                                     Toast.LENGTH_SHORT).show();
 
 
